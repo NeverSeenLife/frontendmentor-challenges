aa55ee75ca75eabab32345c983e295c7b9e7cdab
Neverseenlife <adrian115185@gmail.com>
2023-03-28 23:50:21 +0200

    fix(js repair): clear useless js code

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
-// fetch("data.json")
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (summary) {
-//     let placeholder = document.querySelector("summary-items");
-//     let out = "";
-//     for (let summar of summary) {
-//       out += `
-//         <div class="results-summary-item">
-//         <div class="results-summary-type"><img src='${summar.icon}' alt="" />${summar.category}</div>
-//         <div class="results-summary-percent-container">
-//           <div class="results-summary-percent">${summar.score}</div>
-//           <span>/</span>
-//           <div class="results-summary-max-percent">100</div>
-//         </div>
-//       </div>
-//         `;
-//     }
-//     // placeholder.innerHTML = out;
-//   });
 fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
-    const list = document.querySelector("#list");
+    const list = document.querySelector(".results-summary-info");
+    let out = "";
     data.forEach((item) => {
       const li = document.createElement("li");
-      li.textContent = item.category + " - " + item.score;
-      list.appendChild(li);
+      out += `
+      <div class="results-summary-item">
+        <div class="results-summary-type"><img src='${item.icon}' alt="" />${item.category}</div>
+            <div class="results-summary-percent-container">
+           <div class="results-summary-percent">${item.score}</div>
+           <span>/</span>
+           <div class="results-summary-max-percent">100</div>
+         </div>
+       </div>
+      `;
     });
+    list.innerHTML = out;
   })
   .catch((error) => console.error(error));
